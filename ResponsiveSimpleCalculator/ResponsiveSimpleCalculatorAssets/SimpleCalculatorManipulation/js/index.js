ea905f46cdbf4e0069d5ca461d074ead5659e712
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-09 13:46:38 +0300

    Update index.js

--- ResponsiveSimpleCalculator/ResponsiveSimpleCalculatorAssets/SimpleCalculatorManipulation/js/index.js
+++ ResponsiveSimpleCalculator/ResponsiveSimpleCalculatorAssets/SimpleCalculatorManipulation/js/index.js
@@ -1,47 +1,13 @@
-// var fristHeading = document.querySelector(".responsive-simple-calculator-container .responsive-simple-calculator-heading-container #mainHeading");
-// // console.log(fristHeading);
-// var fristDescription = document.querySelector(".responsive-simple-calculator-container .responsive-simple-calculator-heading-container #mainDescription");
-// // console.log(fristDescription);
-// var userName = window.prompt("Please Enter The User Name Is : ")
-// // console.log(userName);
-// if(!userName){
-//     userName = "guest";
-// }
-// // console.log(userName);
-// function displayingMainDescription(){
-//     fristHeading.addEventListener("click" , function(){
-//         fristDescription.style.display="block";
-//         fristDescription.innerHTML = `hello everyone! , and welcome \"${userName}\" to the responsive simple calculator , you can solve different mathimaticas equations by using simple calculator`;
-//     });
-// };
-// displayingMainDescription();
-// var displayingUserValue = document.querySelector(".responsive-simple-calculator-container .responsive-simple-calculator-buttons-container #displayingValue");
-// // console.log(displayingUserValue);
-// function appendUserValue(displayValue){
-//     displayingUserValue.value += displayValue;
-// };
-// function clearingUserValue(){
-//     displayingUserValue.value="";
-// };
-// function calculatingMathimaticasEquations(){
-//     try{
-//         displayingUserValue.value = eval(displayingUserValue.value);
-//     }
-//     catch{
-//         displayingUserValue.value="mathimaticas syntax error";
-//     };
-// };
-
 var fristHeading = document.getElementById("mainHeading");
-console.log(fristHeading);
+// console.log(fristHeading);
 var fristDescription = document.getElementById("mainDescription");
-console.log(fristDescription);
+// console.log(fristDescription);
 var userName = window.prompt("Please Enter The User Name Is : ");
-console.log(userName);
+// console.log(userName);
 if(!userName){
     userName = "guest";
 };
-console.log(userName);
+// console.log(userName);
 function displayingMainDescription(){
     fristHeading.addEventListener("click" , function(){
         fristDescription.style.display = "block";
@@ -50,7 +16,7 @@ function displayingMainDescription(){
 };
 displayingMainDescription();
 var displayingUserVal = document.getElementById("displayingUserValue");
-console.log(displayingUserVal);
+// console.log(displayingUserVal);
 function appendUserValue(displayingValue){
     displayingUserVal.value += displayingValue;
 }
@@ -60,82 +26,82 @@ function appendUserValue(displayingValue){
 //     console.log(calculatorButtons[i])
 // }
 let divideButton = document.getElementById("divideBtn");
-console.log(divideButton);
+// console.log(divideButton);
 divideButton.addEventListener("click" , function(){
     appendUserValue("/");
 });
 let multiplyButton = document.getElementById("multiplyBtn");
-console.log(multiplyButton);
+// console.log(multiplyButton);
 multiplyButton.addEventListener("click" , function(){
     appendUserValue("*");
 });
 let modulusButton = document.getElementById("modulusBtn");
-console.log(modulusButton);
+// console.log(modulusButton);
 modulusButton.addEventListener("click" , function(){
     appendUserValue("%");
 });
 let additionButton = document.getElementById("additionBtn");
-console.log(additionButton);
+// console.log(additionButton);
 additionButton.addEventListener("click" , function(){
     appendUserValue("+");
 });
 let subtractButton = document.getElementById("subtractBtn");
-console.log(subtractButton);
+// console.log(subtractButton);
 subtractButton.addEventListener("click" , function(){
     appendUserValue("-");
 });
 let dotButton = document.getElementById("dotBtn");
-console.log(dotButton);
+// console.log(dotButton);
 dotButton.addEventListener("click" , function(){
     appendUserValue(".");
 });
 let zeroButton = document.getElementById("zeroBtn");
-console.log(zeroButton);
+// console.log(zeroButton);
 zeroButton.addEventListener("click" , function(){
     appendUserValue("0");
 });
 let oneButton = document.getElementById("oneBtn");
-console.log(oneButton);
+// console.log(oneButton);
 oneButton.addEventListener("click" , function(){
     appendUserValue("1");
 });
 let twoButton = document.getElementById("twoBtn");
-console.log(twoButton);
+// console.log(twoButton);
 twoButton.addEventListener("click" , function(){
     appendUserValue("2");
 });
 let threeButton = document.getElementById("threeBtn");
-console.log(threeButton);
+// console.log(threeButton);
 threeButton.addEventListener("click" , function(){
     appendUserValue("3");
 });
 let fourButton = document.getElementById("fourBtn");
-console.log(fourButton);
+// console.log(fourButton);
 fourButton.addEventListener("click" , function(){
     appendUserValue("4");
 });
 let fiveButton = document.getElementById("fiveBtn");
-console.log(fiveButton);
+// console.log(fiveButton);
 fiveButton.addEventListener("click" , function(){
     appendUserValue("5");
 });
 let sixButton = document.getElementById("sixBtn");
-console.log(sixButton);
+// console.log(sixButton);
 sixButton.addEventListener("click" , function(){
     appendUserValue("6");
 });
 let sevenButton = document.getElementById("sevenBtn");
-console.log(sevenButton);
+// console.log(sevenButton);
 sevenButton.addEventListener("click" , function(){
     appendUserValue("7");
 });
 let eightButton = document.getElementById("eightBtn");
-console.log(eightButton);
+// console.log(eightButton);
 eightButton.addEventListener("click" , function(){
     appendUserValue("8");
 });
 let nineButton = document.getElementById("nineBtn");
-console.log(nineButton);
+// console.log(nineButton);
 nineButton.addEventListener("click" , function(){
     appendUserValue("9");
 });
@@ -143,7 +109,7 @@ function clearingAppendUserValue(){
     displayingUserVal.value="";
 };
 let clearingButton = document.getElementById("clearingBtn");
-console.log(clearingButton);
+// console.log(clearingButton);
 clearingButton.addEventListener("click" , function(){
     clearingAppendUserValue();
 });
@@ -156,11 +122,12 @@ function calculatingDifferentMathimaticasEquations(){
     }
 }
 let equalsButton = document.getElementById("equalsBtn");
-console.log(equalsButton);
+// console.log(equalsButton);
 equalsButton.addEventListener("click" , function(){
     if(displayingUserVal.value.trim() === ""){
         displayingUserVal.value = "enter mathimaticas equation.";
         return;
     }
     calculatingDifferentMathimaticasEquations();
+
 });
